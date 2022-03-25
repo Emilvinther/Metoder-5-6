966f5512cd5079c419a9ba655dd6f3a6299a566c
Emil Lorentzen <emilerher@gmail.com>
2022-03-25 11:06:00 +0100

    Add project files.

---
+++ Metoder 5-6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Metoder_5_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            CreateFile();
+
+            FileStreamRead();
+            FileStreamWrite();
+
+
+            Console.ReadKey();
+
+        }
+
+        public static void CreateFile()
+        {
+            File.WriteAllText(@".\Movies.txt", "Star Wars\nThe Empire Strikes Back\nReturn Of The Jedi");
+            File.WriteAllText(@".\Actors.txt", "");
+        }
+
+        public static void FileStreamRead()
+        {
+            FileStream file = new FileStream(@".\Movies.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(file);
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+
+        }
+
+        public static void FileStreamWrite()
+        {
+            FileStream file = new FileStream(@".\Actors.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(file);
+
+            string[] Actors = { "Mark Hamill", "Harrison Ford", "Carrie Fisher" };
+
+            foreach (string i in Actors)
+            {
+                writer.WriteLine(i);
+                
+            }
+            writer.Close();
+        }
+           
+    }
+
+}
+
